8f78bfb4a7322705c77d7a78f6fa11d44835af47
Chris Allbritton <cla.inbox@gmail.com>
2020-07-29 12:52:55 -0400

    Create vendors_that_sell method and test

--- lib/market.rb
+++ lib/market.rb
@@ -17,7 +17,17 @@ class Market
     vendors.each do |vendor|
       names << vendor.name
     end
-    names 
+    names
+  end
+
+  def vendors_that_sell(item)
+    sells_item = []
+    vendors.each do |vendor|
+      if vendor.inventory.include?(item)
+        sells_item << vendor
+      end
+    end
+    sells_item 
   end
 
 end
