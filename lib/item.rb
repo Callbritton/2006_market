7ec8f520e446fb061e59842edbffae6b5e58cc67
Chris Allbritton <cla.inbox@gmail.com>
2020-07-29 11:27:24 -0400

    Create Vendor class and test it exists

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,10 @@
 class Item
+  attr_reader :name,
+              :price
 
   def initialize(info)
-
+    @name = info[:name]
+    @price = info[:price].to_f 
   end
 
 
--- test/item_test.rb
+++ test/item_test.rb
@@ -10,7 +10,13 @@ class ItemTest < Minitest::Test
     assert_instance_of Item, item1
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_has_attributes
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    assert_equal "Tomato", item2.name
+    assert_equal 0.75, item1.price 
+    assert_equal 0.5, item2.price
+  end
 
 end
