4b7c49f2c96671ca83b90697a9de0275ab84435d
Chris Allbritton <cla.inbox@gmail.com>
2020-07-29 12:24:48 -0400

    Create working test_it_can_stock_items test and methods

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -32,16 +32,18 @@ class VendorTest < Minitest::Test
   def test_it_can_stock_items_in_inventory
     vendor = Vendor.new("Rocky Mountain Fresh")
     item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
     vendor.stock(item1, 30)
+    vendor.stock(item1, 25)
 
     expected = {
-      item1 => 30
+      item1 => 55,
+      item2 => 12
     }
+
+    vendor.stock(item2, 12)
+    assert_equal 55, vendor.check_stock(item1)
     assert_equal expected, vendor.inventory
-    assert_equal 30, vendor.check_stock(item1)
-
-    # vendor.stock(item1, 25)
-
   end
 
 end
